b1faf07ec23c9d40606860953f4ac6d7d18ffa9f
sunny zhao <sunny_zhao@cate.org>
2020-01-16 11:19:11 -0800

    Initial Commit

---
+++ Bobby builder project/ContentView.swift
@@ -0,0 +1,22 @@
+
+//
+//  ContentView.swift
+//  Bobby builder project
+//
+//  Created by Sunny Zhao on 1/16/20.
+//  Copyright © 2020 Sunny Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
